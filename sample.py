ccf50d28b4d4fc82c048cca602444cc99ab299ad
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2025-11-14 16:03:15 +0100

    Added sample

--- sample.py
+++ sample.py
@@ -1,8 +1,15 @@
 # -*- coding: utf-8 -*-
+from PyPDF2 import PdfReader
 import ollama
 
+reader = PdfReader("data/sample.pdf")
+content = "\n".join(page.extract_text() for page in reader.pages)
+
 resp = ollama.chat(model="llama3.1:8b", messages=[
-    {"role": "user", "content": "Explain quantum entanglement simply."}
+    {
+        "role": "user",
+        "content": f"Summarize the following: {content}."
+    }
 ])
 
 print(resp["message"]["content"])
